d8ec84bed14bf5021db63b3c9d50c5fe4c65f9ac
Rachid <rachidboubekeur@free.fr>
2021-03-01 11:37:57 +0100

    ci(div1,div2,div3): développement des div1 ,2 et 3

--- indexJS.js
+++ indexJS.js
@@ -1,12 +1,29 @@
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 300) {
+    if(window.scrollY > 665) {
         document.querySelector('header').style.opacity = 1
     } else {
         document.querySelector('header').style.opacity = 0
     }
-    if(window.scrollY > 400) {
-        document.querySelector('#div1').style.left = "19%"
+    if(window.scrollY > 400 && window.scrollY < 1150) {
+        document.querySelector('#div1').style.marginLeft = "0%"
+        document.querySelector('#div1').style.opacity = 1
     } else {
-        document.querySelector('#div1').style.left = "-67%"
+        document.querySelector('#div1').style.marginLeft = "-166%"
+        document.querySelector('#div1').style.opacity = 0
+    }
+    if(window.scrollY > 1050 && window.scrollY < 2000) {
+        document.querySelector('#div2').style.marginTop = "10%"
+        document.querySelector('#div2').style.opacity = 1
+    } else {
+        document.querySelector('#div2').style.marginTop = "0%"
+        document.querySelector('#div2').style.opacity = 0
+    }
+
+    if(window.scrollY > 1800) {
+        document.querySelector('#div3').style.marginTop = "10%"
+        document.querySelector('#div3').style.opacity = 1
+    } else {
+        document.querySelector('#div3').style.marginTop = "0%"
+        document.querySelector('#div3').style.opacity = 0
     }
 })
